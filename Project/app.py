2b3b7fc0677a3005c2329f55b0c9ef3934cf37fa
MrBlu3Sky57 <tudor.avram@icloud.com>
2023-09-16 16:56:42 -0400

    added ZK proofs workshop code

--- Project/app.py
+++ Project/app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
 from helpers import *
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
